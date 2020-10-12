b20bc8f80ae8f941645670fdd59227cba95be378
nguyendacthienngan <nguyendacthienngan@gmail.com>
2020-10-12 12:52:25 +0700

    Init Camera class

--- SuperMarioBros3/SuperMarioBros3/Camera.cpp
+++ SuperMarioBros3/SuperMarioBros3/Camera.cpp
@@ -1,1 +1,84 @@
 #include "Camera.h"
+
+CCamera::CCamera(int wid, int hei)
+{
+    widthCam = wid;
+    heightCam = hei;
+    boundaryLeft = 0;
+    boundaryRight = 0;
+    vx = 0.0f;
+}
+
+CCamera::~CCamera()
+{
+}
+
+void CCamera::Update()
+{
+}
+
+D3DXVECTOR2 CCamera::Transform(D3DXVECTOR2 posWorld)
+{
+    return D3DXVECTOR2(posWorld.x - posCam.x, posWorld.y - posCam.y);
+}
+
+void CCamera::CheckObjectInCamera()
+{
+}
+
+int CCamera::GetSpeedXCam()
+{
+    return vx;
+}
+
+D3DXVECTOR2 CCamera::GetPositionCam()
+{
+    return posCam;
+}
+
+int CCamera::GetWidthCam()
+{
+    return widthCam;
+}
+
+int CCamera::GetHeightCam()
+{
+    return heightCam;
+}
+
+float CCamera::GetBoundaryLeft()
+{
+    return boundaryLeft;
+}
+
+float CCamera::GetBoundaryRight()
+{
+    return boundaryRight;
+}
+
+void CCamera::SetSpeedXCam(float v)
+{
+    vx = v;
+}
+
+void CCamera::SetPositionCam(D3DXVECTOR2 pos)
+{
+    posCam.x = pos.x;
+    posCam.y = pos.y;
+}
+
+void CCamera::SetWidthCam(int w)
+{
+    widthCam = w;
+}
+
+void CCamera::SetHeightCam(int h)
+{
+    heightCam = h;
+}
+
+void CCamera::SetBoundary(float left, float right)
+{
+    boundaryLeft = left;
+    boundaryRight = right;
+}
--- SuperMarioBros3/SuperMarioBros3/Camera.h
+++ SuperMarioBros3/SuperMarioBros3/Camera.h
@@ -1,5 +1,34 @@
-#pragma once
-class Camera
+﻿#pragma once
+#include <d3dx9.h>
+class CCamera
 {
+private:
+	float vx;
+	D3DXVECTOR2 posCam;
+	int widthCam;
+	int heightCam;
+
+	float boundaryLeft; // set biên camera dựa vào kích thước map: boundary.left = 0
+	float boundaryRight; // VD: boundary.right =  map.width - cam.width => Tùy theo cách mình xét, đây chỉ là ví dụ
+public:
+	CCamera(int wid, int hei);
+	~CCamera();
+	void Update();
+	D3DXVECTOR2 Transform(D3DXVECTOR2 posWorld);
+	void CheckObjectInCamera(); //Kiểm tra xem obj truyền vào có nằm trong camera không để ta xử lý update / renderm
+
+	int GetSpeedXCam();
+	D3DXVECTOR2 GetPositionCam();
+	int GetWidthCam();
+	int GetHeightCam();
+	float GetBoundaryLeft();
+	float GetBoundaryRight();
+
+	void SetSpeedXCam(float v);
+	void SetPositionCam(D3DXVECTOR2 pos);
+	void SetWidthCam(int w);
+	void SetHeightCam(int h);
+	void SetBoundary(float left, float right);
+
 };
 
