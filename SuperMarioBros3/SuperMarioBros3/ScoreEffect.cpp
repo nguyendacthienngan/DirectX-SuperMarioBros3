230bb1a3de98cd9e9147fdbdef73b0545bf26de3
nguyendacthienngan <nguyendacthienngan@gmail.com>
2020-12-20 20:19:12 +0700

    Changing scoreFX sprites

--- SuperMarioBros3/SuperMarioBros3/ScoreEffect.cpp
+++ SuperMarioBros3/SuperMarioBros3/ScoreEffect.cpp
@@ -2,12 +2,12 @@
 #include "ScoreFXConst.h"
 #include "Game.h"
 #include "UICamera.h"
+#include "AnimationManager.h"
 CScoreEffect::CScoreEffect()
 {
-	scoreText = new CFont();
-	scoreText->SetCurrentText("100");
+	LoadAnimation();
 	currentScore = 100;
-	scoreText->SetSpace(D3DXVECTOR2(8*3, 0));
+	SetState("100");
 	physiscBody->SetGravity(SCORE_FX_GRAVITY);
 	physiscBody->SetVelocity(D3DXVECTOR2(0.0f, -SCORE_FX_VELOCITY));
 	physiscBody->SetDynamic(false);
@@ -15,15 +15,23 @@ CScoreEffect::CScoreEffect()
 	scoreState = 0;
 }
 
-void CScoreEffect::SetStartPosition(D3DXVECTOR2 pos)
+void CScoreEffect::LoadAnimation()
 {
-	CEffectObject::SetStartPosition(pos);
+	auto animationManager = CAnimationManager::GetInstance();
+	AddAnimation("100", animationManager->Get("ani-points-100"));
+	AddAnimation("200", animationManager->Get("ani-points-200"));
+	AddAnimation("400", animationManager->Get("ani-points-400"));
+	AddAnimation("800", animationManager->Get("ani-points-800"));
+	AddAnimation("1000", animationManager->Get("ani-points-1000"));
+	AddAnimation("2000", animationManager->Get("ani-points-2000"));
+	AddAnimation("4000", animationManager->Get("ani-points-4000"));
+	AddAnimation("8000", animationManager->Get("ani-points-8000"));
+	AddAnimation("1UP", animationManager->Get("ani-points-1UP"));
 }
+
 
 void CScoreEffect::Update(DWORD dt, CCamera* cam, CCamera* uiCam)
 {
-	if (scoreText == NULL)
-		return;
 	scoreState++;
 	transform.position.y -= SCORE_FX_VELOCITY * dt;
 	if (uiCam != NULL && scoreState == 1)
@@ -39,7 +47,5 @@ void CScoreEffect::Update(DWORD dt, CCamera* cam, CCamera* uiCam)
 
 void CScoreEffect::Render(CCamera* cam, int alpha)
 {
-	auto camPos = cam->Transform(GetPosition());
-	scoreText->SetPosition(camPos);
-	scoreText->Render();
+	CGameObject::Render(cam, alpha);
 }
--- SuperMarioBros3/SuperMarioBros3/ScoreEffect.h
+++ SuperMarioBros3/SuperMarioBros3/ScoreEffect.h
@@ -3,12 +3,11 @@
 #include "Font.h"
 class CScoreEffect : public CEffectObject
 {
-	CFont* scoreText;
 	int currentScore;
 	int scoreState;
 public:
 	CScoreEffect();
-	void SetStartPosition(D3DXVECTOR2 pos);
+	void LoadAnimation() override;
 
 	void Update(DWORD dt, CCamera* cam, CCamera* uiCam) override;
 	void Render(CCamera* cam, int alpha = 255) override;
