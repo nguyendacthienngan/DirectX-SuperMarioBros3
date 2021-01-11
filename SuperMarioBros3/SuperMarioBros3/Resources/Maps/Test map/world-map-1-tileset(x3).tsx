1e63903de3349cb8161858801c227a40eb010197
Nguyen Dac Thien Ngan <54399397+nguyendacthienngan@users.noreply.github.com>
2021-01-11 15:13:07 +0700

    Restructure resources Adding new map : Scene 1-4

--- SuperMarioBros3/SuperMarioBros3/Resources/Maps/Test map/world-map-1-tileset(x3).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="world-map-1-tileset(x3)" tilewidth="48" tileheight="48" tilecount="24" columns="8">
- <image source="world-map-1-tileset(x3).png" width="384" height="144"/>
-</tileset>
