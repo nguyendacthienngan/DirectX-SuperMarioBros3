60a6c1a8eff23536108180a7215ef2bd39cb8af0
Nguyen Dac Thien Ngan <54399397+nguyendacthienngan@users.noreply.github.com>
2021-01-16 15:10:54 +0700

    Fixing map const

---
+++ SuperMarioBros3/SuperMarioBros3/Grid.h
@@ -0,0 +1,9 @@
+#pragma once
+class CGrid
+{
+private:
+
+public:
+
+};
+
--- SuperMarioBros3/SuperMarioBros3/MapConst.h
+++ SuperMarioBros3/SuperMarioBros3/MapConst.h
@@ -1,9 +1,19 @@
 ﻿#pragma once
 const D3DXVECTOR2 translateConst = D3DXVECTOR2(24, 40);
-const D3DXVECTOR2 translateGoombaConst = D3DXVECTOR2(24, 50); // 50 hoặc 43
-const D3DXVECTOR2 translateKoopaConst = D3DXVECTOR2(24, 60); // 60 hoặc 59
-const D3DXVECTOR2 translateKoopaShellConst = D3DXVECTOR2(24, 45); // 43 hoặc 45 (1181)
-const D3DXVECTOR2 translateQuestionBlockConst = D3DXVECTOR2(24, 41.5);
-const D3DXVECTOR2 translatePiranhaConst = D3DXVECTOR2(24, 66);
-const D3DXVECTOR2 translateVenusConst = D3DXVECTOR2(24, 66);
-const D3DXVECTOR2 translateGrassConst = D3DXVECTOR2(23, 40.5);
+//const D3DXVECTOR2 translateGoombaConst = D3DXVECTOR2(0, 0);
+//const D3DXVECTOR2 translateKoopaConst = D3DXVECTOR2(0, 0);
+//const D3DXVECTOR2 translateKoopaShellConst = D3DXVECTOR2(0, 0);
+//const D3DXVECTOR2 translateQuestionBlockConst = D3DXVECTOR2(0, 0);
+//const D3DXVECTOR2 translatePiranhaConst = D3DXVECTOR2(0, 0);
+//const D3DXVECTOR2 translateVenusConst = D3DXVECTOR2(0, 0);
+//const D3DXVECTOR2 translateGrassConst = D3DXVECTOR2(0, 0);
+
+
+//const D3DXVECTOR2 translateConst = D3DXVECTOR2(24, 40);
+//const D3DXVECTOR2 translateGoombaConst = D3DXVECTOR2(24, 50); // 50 hoặc 43
+//const D3DXVECTOR2 translateKoopaConst = D3DXVECTOR2(24, 60); // 60 hoặc 59
+//const D3DXVECTOR2 translateKoopaShellConst = D3DXVECTOR2(24, 45); // 43 hoặc 45 (1181)
+//const D3DXVECTOR2 translateQuestionBlockConst = D3DXVECTOR2(24, 41.5);
+//const D3DXVECTOR2 translatePiranhaConst = D3DXVECTOR2(24, 66);
+//const D3DXVECTOR2 translateVenusConst = D3DXVECTOR2(24, 66);
+//const D3DXVECTOR2 translateGrassConst = D3DXVECTOR2(23, 40.5);
